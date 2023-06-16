8cb93683cf698ad4577b8bd66cdebc6d5543fb4a
Joel Parra <95077198+jopar210@users.noreply.github.com>
2023-06-16 21:09:07 +0200

    Add basic logic

---
+++ main.js
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////
+// VARIABLES
+////////////////////////////////////////////////////////
+
+let fistNumber;
+let operator;
+let secondNumber;
+
+////////////////////////////////////////////////////////
+// FUNCTIONS
+////////////////////////////////////////////////////////
+
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+const operate = function (num1, operator, num2) {
+  switch (operator) {
+    case "+": {
+      return add(num1, num2);
+    }
+    case "-": {
+      return subtract(num1, num2);
+    }
+    case "*": {
+      return multiply(num1, num2);
+    }
+    case "/": {
+      return divide(num1, num2);
+    }
+  }
+};
