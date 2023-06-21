c7ef86855bfeb439884a3798a2c88fe0bc14d73f
Joel Parra <95077198+jopar210@users.noreply.github.com>
2023-06-21 21:56:05 +0200

    Do HTML structure and add gradient background

--- main.js
+++ main.js
@@ -26,6 +26,12 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
+const clear = function () {
+  fistNumber = 0;
+  operator = null;
+  secondNumber = 0;
+};
+
 const operate = function (num1, operator, num2) {
   switch (operator) {
     case "+": {
